096fbcdf7a9aa57194639a1366be42d9023aed60
mahmudov2001 <mahmudovnurlan301@gmail.com>
2024-09-06 18:46:20 +0400

    updated search component and add select options and calendar design

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,36 +1,102 @@
-import '../search/search.scss'
+import '../search/search.scss';
 import { useState } from 'react';
-import { VscChevronDown } from "react-icons/vsc";
+import { VscChevronDown, VscChevronUp } from "react-icons/vsc";
 import { IoIosSearch } from "react-icons/io";
 import { FiRefreshCcw } from "react-icons/fi";
-import { VscChevronUp } from "react-icons/vsc";
+
 function Search() {
     const [bool, setBool] = useState(false);
-    function showMore(){
-        setBool(!bool)
+    
+    function showMore() {
+        setBool(!bool);
     }
-  return (
-    <>
-        <div className="searchContainer">
-            <div className="filter">
-                <div className='myInput'>
-                <input type="text" placeholder='Satınalma predmeti' />
-                </div>
-                <div className='more' onClick={showMore}>
-                    <span>Ətraflı axtarış</span>
-                   {bool ?<VscChevronUp className='upDown'/>  : <VscChevronDown className='upDown'/>} 
-                    
 
-                </div>
-                <div className="buttons">
-                    <button><IoIosSearch /></button>
-                    <button><FiRefreshCcw /></button>
+    return (
+        <>
+            <div className="searchContainer">
+                <div className="filter">
+                    <div className='myInput'>
+                        <input type="text" placeholder='Satınalma predmeti' /> 
+                    </div>
+                    <div className='more' onClick={showMore}>
+                        <span>Filter</span>
+                        {bool ? <VscChevronUp className='upDown' /> : <VscChevronDown className='upDown' />}
+                    </div>
+                    <div className="buttons">
+                        <button><IoIosSearch /></button>
+                        <button><FiRefreshCcw /></button>
+                    </div>
                 </div>
             </div>
-
-        </div>
-    </>
-  )
+            {bool && (
+                <div className="filterContainer">
+                    <div className="filterModal">
+                        <p>Şəhər seçin:</p>
+                        <select name="" id="">
+                            <option value="" selected disabled>Seçim edin</option>
+                            <option value="">Bakı</option>
+                            <option value="">Cəlilabad</option>
+                            <option value="">Gəncə</option>
+                            <option value="">Kürdəmir</option>
+                            <option value="">Naxçıvan</option>
+                            <option value="">Zaqatala</option>
+                        </select>
+                        <p>Qiymət:</p>
+                        <input type="number" placeholder='Min' />
+                        <input type="number" placeholder='Max' />
+                        <div className="calendar">
+                            <div className="month">
+                                <button>&#8249;</button>
+                                <h1>Sentyabr 2024</h1>
+                                <button>&#8250;</button>
+                            </div>
+                            <div className="weekdays">
+                                <div>B.e</div>
+                                <div>Ç.a</div>
+                                <div>Ç</div>
+                                <div>C.a</div>
+                                <div>C</div>
+                                <div>Ş</div>
+                                <div>B</div>
+                            </div>
+                            <div className="days">
+                                <div>1</div>
+                                <div>2</div>
+                                <div>3</div>
+                                <div>4</div>
+                                <div>5</div>
+                                <div>6</div>
+                                <div>7</div>
+                                <div>8</div>
+                                <div>9</div>
+                                <div>10</div>
+                                <div>11</div>
+                                <div>12</div>
+                                <div>13</div>
+                                <div>14</div>
+                                <div>15</div>
+                                <div>16</div>
+                                <div>17</div>
+                                <div>18</div>
+                                <div>19</div>
+                                <div>20</div>
+                                <div>21</div>
+                                <div className="current-day">22</div>
+                                <div>23</div>
+                                <div>24</div>
+                                <div>25</div>
+                                <div>26</div>
+                                <div>27</div>
+                                <div>28</div>
+                                <div>29</div>
+                                <div>30</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
 
-export default Search
+export default Search;
