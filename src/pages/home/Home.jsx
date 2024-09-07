7fa0b5c989bf12e0161c4a8a90f8106931402782
Arifdsh <19arifvm93@gmail.com>
2024-09-07 21:32:30 +0400

    Conflicts resolved

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import  Navbar  from '../../components/navbar/Navbar.jsx'
 import Search from '../../components/search/Search.jsx'
 import Cards from '../../components/cards/Cards.jsx'
+import Hero from '../../components/hero/Hero.jsx'
 
 const Home = () => {
   return (
