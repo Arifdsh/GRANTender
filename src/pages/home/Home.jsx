17a48c17767abb0edc85f065a2b9186f29cc1e56
Arifdsh <19arifvm93@gmail.com>
2024-09-07 18:33:29 +0400

    Hero section created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import  Navbar  from '../../components/navbar/Navbar.jsx'
 import Search from '../../components/search/Search.jsx'
+import Hero from '../../components/hero/Hero.jsx'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
       <Search/>
     </div>
   )
