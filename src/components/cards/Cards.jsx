68421eaa9232bc0f5a95cb93554402d73df1474a
mahmudov2001 <mahmudovnurlan301@gmail.com>
2024-09-08 18:06:12 +0400

    cards component design finished

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -7,19 +7,40 @@ function Cards() {
         <div className="itemLeft">
           <img src={Car} alt="" />
         </div>
-        <div className="itemRight"></div>
+        <div className="itemRight">
+          <h1>Elan Sahibi:<span>Nurlan Holding</span></h1>
+          <h1>Elanın predmeti: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ad?</span></h1>
+          <h1>Elanın yaradılış tarixi: <span className='createSpans'>03/07/2024</span></h1>
+          <h1>Elanın bitmə tarixi: <span className='createSpans' >15/12/2024</span></h1>
+          <button>Ətraflı</button>
+          <button className='editTender'>Sil</button>
+          <button className='editTender'>Düzəliş et</button>
+
+        </div>
       </div>
       <div className="cardsItem">
         <div className="itemLeft">
           <img src={Car} alt="" />
         </div>
-        <div className="itemRight"></div>
+        <div className="itemRight">
+          <h1>Elan Sahibi:<span>Nurlan Holding</span></h1>
+          <h1>Elanın predmeti: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ad?</span></h1>
+          <h1>Elanın yaradılış tarixi: <span className='createSpans'>03/07/2024</span></h1>
+          <h1>Elanın bitmə tarixi: <span className='createSpans' >15/12/2024</span></h1>
+          <button>Ətraflı</button>
+        </div>
       </div>
       <div className="cardsItem">
         <div className="itemLeft">
           <img src={Car} alt="" />
         </div>
-        <div className="itemRight"></div>
+        <div className="itemRight">
+          <h1>Elan Sahibi:<span>Nurlan Holding</span></h1>
+          <h1>Elanın predmeti: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ad?</span></h1>
+          <h1>Elanın yaradılış tarixi: <span className='createSpans'>03/07/2024</span></h1>
+          <h1>Elanın bitmə tarixi: <span className='createSpans' >15/12/2024</span></h1>
+          <button>Ətraflı</button>
+        </div>
       </div>
       </div>
 
