7f3ed39fa3f7485d439fe2b1b8d855586d0206bd
Arifdsh <19arifvm93@gmail.com>
2024-09-10 00:09:53 +0400

    Create authorization and formik

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import  Navbar  from '../../components/navbar/Navbar.jsx'
 import Search from '../../components/search/Search.jsx'
 import Cards from '../../components/cards/Cards.jsx'
 import Hero from '../../components/hero/Hero.jsx'
@@ -7,7 +6,6 @@ import Hero from '../../components/hero/Hero.jsx'
 const Home = () => {
   return (
     <div>
-      <Navbar/>
       <Hero/>
       <Search/>
       <Cards/>
