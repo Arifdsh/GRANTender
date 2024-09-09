406c72ed5933ea75bc4c6c53509f42978c29f20b
mahmudov2001 <mahmudovnurlan301@gmail.com>
2024-09-09 16:48:50 +0400

    db.json file's add  cards information

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,6 +1,15 @@
+import React, { useState } from 'react';
 import '../cards/cards.scss'
 import Car from '../../assets/image/car.jpg'
+import { FaBookmark, FaRegBookmark } from "react-icons/fa";
+
 function Cards() {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const handleBookmarkClick = () => {
+    setIsBookmarked(!isBookmarked);
+  };
+
   return (
     <div className="cardsContainer">
       <div className="cardsItem">
@@ -8,14 +17,23 @@ function Cards() {
           <img src={Car} alt="" />
         </div>
         <div className="itemRight">
-          <h1>Elan Sahibi:<span>Nurlan Holding</span></h1>
-          <h1>Elanın predmeti: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ad?</span></h1>
-          <h1>Elanın yaradılış tarixi: <span className='createSpans'>03/07/2024</span></h1>
-          <h1>Elanın bitmə tarixi: <span className='createSpans' >15/12/2024</span></h1>
-          <button>Ətraflı</button>
-          <button className='editTender'>Sil</button>
-          <button className='editTender'>Düzəliş et</button>
-
+          <div className="about">
+            <h1>Elan Sahibi:<span>Nurlan Holding</span></h1>
+            <h1>Elanın predmeti: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ad?</span></h1>
+            <h1>Elanın yaradılış tarixi: <span className='createSpans'>03/07/2024</span></h1>
+            <h1>Elanın bitmə tarixi: <span className='createSpans' >15/12/2024</span></h1>
+          </div>
+          <div className='arrow'></div>
+          <div className="editDelete">
+            <button className='editDeleteButtons'>Sil</button>
+            <button className='editDeleteButtons'>Düzəliş et</button>
+            <button>Ətraflı</button>
+          </div>
+          <div>
+            <div onClick={handleBookmarkClick} className='save'>
+              {isBookmarked ? <FaBookmark /> : <FaRegBookmark />}
+            </div>
+          </div>
         </div>
       </div>
       <div className="cardsItem">
@@ -23,11 +41,23 @@ function Cards() {
           <img src={Car} alt="" />
         </div>
         <div className="itemRight">
-          <h1>Elan Sahibi:<span>Nurlan Holding</span></h1>
-          <h1>Elanın predmeti: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ad?</span></h1>
-          <h1>Elanın yaradılış tarixi: <span className='createSpans'>03/07/2024</span></h1>
-          <h1>Elanın bitmə tarixi: <span className='createSpans' >15/12/2024</span></h1>
-          <button>Ətraflı</button>
+          <div className="about">
+            <h1>Elan Sahibi:<span>Nurlan Holding</span></h1>
+            <h1>Elanın predmeti: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ad?</span></h1>
+            <h1>Elanın yaradılış tarixi: <span className='createSpans'>03/07/2024</span></h1>
+            <h1>Elanın bitmə tarixi: <span className='createSpans' >15/12/2024</span></h1>
+          </div>
+          <div className='arrow'></div>
+          <div className="editDelete">
+            <button className='editDeleteButtons'>Sil</button>
+            <button className='editDeleteButtons'>Düzəliş et</button>
+            <button>Ətraflı</button>
+          </div>
+          <div>
+            <div onClick={handleBookmarkClick} className='save'>
+              {isBookmarked ? <FaBookmark /> : <FaRegBookmark />}
+            </div>
+          </div>
         </div>
       </div>
       <div className="cardsItem">
@@ -35,16 +65,27 @@ function Cards() {
           <img src={Car} alt="" />
         </div>
         <div className="itemRight">
-          <h1>Elan Sahibi:<span>Nurlan Holding</span></h1>
-          <h1>Elanın predmeti: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ad?</span></h1>
-          <h1>Elanın yaradılış tarixi: <span className='createSpans'>03/07/2024</span></h1>
-          <h1>Elanın bitmə tarixi: <span className='createSpans' >15/12/2024</span></h1>
-          <button>Ətraflı</button>
+          <div className="about">
+            <h1>Elan Sahibi:<span>Nurlan Holding</span></h1>
+            <h1>Elanın predmeti: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ad?</span></h1>
+            <h1>Elanın yaradılış tarixi: <span className='createSpans'>03/07/2024</span></h1>
+            <h1>Elanın bitmə tarixi: <span className='createSpans' >15/12/2024</span></h1>
+          </div>
+          <div className='arrow'></div>
+          <div className="editDelete">
+            <button className='editDeleteButtons'>Sil</button>
+            <button className='editDeleteButtons'>Düzəliş et</button>
+            <button>Ətraflı</button>
+          </div>
+          <div>
+            <div onClick={handleBookmarkClick} className='save'>
+              {isBookmarked ? <FaBookmark /> : <FaRegBookmark />}
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-
+    </div>
   )
 }
 
-export default Cards
+export default Cards;
