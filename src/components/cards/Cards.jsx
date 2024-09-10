bd82e7614216226f7f6c3c0bae879564af448d83
mahmudov2001 <mahmudovnurlan301@gmail.com>
2024-09-10 12:33:16 +0400

    import correct cards.jsx

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../cards/cards.scss'
 import Car from '../../assets/image/car.jpg'
