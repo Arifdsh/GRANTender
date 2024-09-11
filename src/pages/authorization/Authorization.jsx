a3dd7f10baec3b9ab97653a35805eeb62700c125
kubik6 <mursukayevr@gmail.com>
2024-09-11 18:21:30 +0400

    connected userslice to autorization and corrected registration

--- src/pages/authorization/Authorization.jsx
+++ src/pages/authorization/Authorization.jsx
@@ -1,30 +1,43 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Formik, useFormik } from 'formik';
 import { AuthorizationSchema } from './AuthorizationSchema.js'
 import './authorization.scss'
+import { useDispatch } from 'react-redux';
+import { updateUser } from '../../features/usersSlice.js';
 
 const Authorization = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const dispatch = useDispatch()
+
   const openModal = (e) =>{
     e.preventDefault(); 
     setIsModalOpen(true)
   } 
   const closeModal = () => setIsModalOpen(false);
 
-  const { values, handleChange, errors, handleSubmit, touched } = useFormik({
+  const { values, handleChange, errors, handleSubmit, touched, resetForm } = useFormik({
     initialValues: {
       name: '',
       surname: '',
       email: '',
       age: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
     },
     onSubmit: (values, actions) => {
-      actions.resetForm()
+      const { confirmPassword, ...userData } = values
+      dispatch(updateUser(userData))
+        .then(() => {
+          actions.resetForm();
+          closeModal();
+        })
+        .catch((error) => {
+          console.error('Failed to update user:', error);
+        });
     },
     validationSchema: AuthorizationSchema
   });
+
   return (
     <div>
       <section className='authorization'>
@@ -51,14 +64,16 @@ const Authorization = () => {
           <form className="register" onSubmit={handleSubmit}>
             <input name='name' className="register__name input" type="text" placeholder="Name" value={values.name} onChange={handleChange}/>
             {errors.name && touched.name && <div className='error'>{errors.name}</div>}
-            <input name='surname' className="register__surname input" type="text" placeholder="Surname" onChange={handleChange}/>
+            <input name='surname' className="register__surname input" type="text" placeholder="Surname" value={values.surname} onChange={handleChange}/>
             {errors.surname && touched.surname && <div className='error'>{errors.surname}</div>}
-            <input name='email' className="register__email input" type="email" placeholder="Email" onChange={handleChange} />
+            <input name='email' className="register__email input" type="email" placeholder="Email" value={values.email} onChange={handleChange} />
             {errors.email && touched.email && <div className='error'>{errors.email}</div>}
-            <input name='age' className="register__dateTime input" type="date" placeholder="Birth date" onChange={handleChange}/>
+            <input name='age' className="register__dateTime input" type="date" placeholder="Birth date" value={values.age} onChange={handleChange}/>
             {errors.age && touched.age && <div className='error'>{errors.age}</div>}
-            <input className="register__password input" type="password" placeholder=" Set password" />
-            <input className="register__confirmPassword input" type="password" placeholder=" Confirm password" />
+            <input name='password' className="register__password input" type="password" placeholder=" Set password" value={values.password} onChange={handleChange}/>
+            {errors.password && touched.password && <div className='error'>{errors.password}</div>} 
+            <input name='confirmPassword' className="register__confirmPassword input" type="password" placeholder=" Confirm password" value={values.confirmPassword} onChange={handleChange}/>
+            {errors.confirmPassword && touched.confirmPassword && <div className='error'>{errors.confirmPassword}</div>}
           <button type='submit' className="register__button">Register</button>
           </form>
           {/* <i className="fa-solid fa-circle-xmark"></i> */}
