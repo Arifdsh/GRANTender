0b72da35df1a809b1ca594dc53e164e08242b354
Arifdsh <19arifvm93@gmail.com>
2024-09-20 18:06:42 +0400

    Edit applyCard scss

--- src/components/applyCard/ApplyCard.jsx
+++ src/components/applyCard/ApplyCard.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import './applyCard.scss'
 import { FaUserTie } from "react-icons/fa";
+import { FaChevronCircleLeft, FaChevronCircleDown } from "react-icons/fa";
+import { } from "react-icons/fa";
 const ApplyCard = () => {
 
     const [isActive, setIsActive] = useState(false);
 
     const toggleAccordion = () => {
-      setIsActive((prevState) => !prevState);
+        setIsActive((prevState) => !prevState);
     };
     return (
         <div className="appeal">
@@ -18,7 +20,10 @@ const ApplyCard = () => {
                             <h6 className="appeal-list__profile--name">Ali</h6>
                         </div>
                         <p className="appeal-list__profile--tender">Tender adı: Bina</p>
-                        <button onClick={toggleAccordion} className="appeal-list__profile--detail">Ətraflı</button>
+                        {!isActive ?
+                            (<FaChevronCircleLeft onClick={toggleAccordion} className="appeal-list__profile--detail" />
+                            )
+                            : <FaChevronCircleDown onClick={toggleAccordion} className="appeal-list__profile--detail" />}
                     </div>
                     <div className={`appeal-list__information ${isActive ? "activeAccordion" : ""}`}>
                         <div className="appeal-list__company">
